403f84b115a0d05430b81d41baf5d10709337439
Dmitrii Valuev <dima.vap2013@gmail.com>
2025-03-16 16:39:53 +0200

    configure

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      formats: ['image/avif', 'image/webp'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'solvy-static.nyc3.cdn.digitaloceanspaces.com',
+          pathname: '/images/**',
+        },
+      ],
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512, 1024, 2048],
+    },
+  };  
+  
+  export default nextConfig;
+  
